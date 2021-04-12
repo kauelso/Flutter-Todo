95f474df6c360aca839193189e325d531f73d308
Kaue Oliveira <kauelucas@terra.com.br>
2021-04-12 08:41:02 -0300

    First Commit

---
+++ android/app/src/main/java/com/example/lista_de_atividades/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.lista_de_atividades;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lista de Atividades',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Lista de Atividades'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class Activity {
+  var nome;
+  var descricao;
+  var saved;
+
+  Activity(String nomeIn,String descricaoIn){
+    nome = nomeIn;
+    descricao = descricaoIn;
+    saved = false;
+  }
+
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _activityList = <Activity>[];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addActivityWindow,
+        tooltip: 'New Activity',
+        child: Icon(Icons.add),
+      ),
+      body: _listWidget(),
+    );
+  }
+
+  Widget _listWidget(){
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: (context,i){
+          if(i.isOdd) return Divider();
+
+          final index = i~/2;
+
+          if(index < _activityList.length) return _activityListBuilder(_activityList[index]);
+
+          return null;
+
+
+        },
+    );
+  }
+
+  Widget _activityListBuilder(Activity item){
+    return ListTile(
+      title: Text(item.nome),
+      onTap: ()=>_showDetails(item),
+      trailing:  IconButton(
+       icon: Icon(
+          item.saved ? Icons.check_box : Icons.check_box_outline_blank,
+          color: item.saved ? Colors.green : Colors.grey,
+        ),
+        onPressed: (){
+         item.saved = true;
+        },
+      ),
+    );
+  }
+
+  void _showDetails(Activity item){
+    Navigator.of(context).push(
+      MaterialPageRoute(
+          builder: (BuildContext context){
+            return Scaffold(
+              appBar: AppBar(leading: BackButton(color: Colors.white,onPressed: () => Navigator.pop(context)),),
+              body: ,
+            );
+          }
+      )
+
+    );
+  }
+
+  void _addActivityWindow(){
+    final _formKey = GlobalKey<FormState>();
+    var _activityName;
+    var _activityDescription = "";
+  Navigator.of(context).push(
+  MaterialPageRoute(
+      builder: (BuildContext context){
+        return Scaffold(
+          appBar: AppBar(title: Text("Nova Atividade",)),
+          body: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: (InputDecoration(
+                    labelText: "Nome da Tarefa",
+                    icon: Icon(Icons.lightbulb_outline),
+                  )
+                  ),
+                  onSaved: (String value){
+                    _activityName = value;
+                  },
+                  validator: (String value){
+                    return (value == null || value.isEmpty ? "Campo Obrigatorio" : null);
+                  },
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.multiline,
+                  minLines: 1,
+                  maxLines: 6,
+                  decoration: (InputDecoration(
+                    labelText: "Descrição da Tarefa",
+                    icon: Icon(Icons.list_alt),
+                  )
+                  ),
+                  onSaved: (String value){
+                    _activityDescription = value;
+                  },
+                ),
+                ElevatedButton(
+                    onPressed: (){
+                      if(_formKey.currentState.validate()){
+                        _formKey.currentState.save();
+                        _activityList.add(new Activity(_activityName, _activityDescription));
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(SnackBar(content: Text(_activityName + " adicionada")));
+                        Navigator.pop(context);
+                      }
+                    },
+                    child: Text("Adicionar atividade"))
+              ],
+            ),
+          )
+        );
+      })
+  );
+  }
+}
